8e149f81b870bde52bf9fdf3463811ff76653103
ernie <ernie@epicbytes.net>
2013-05-21 01:15:14 +0200

    added ent decoding and sorting

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,13 @@
  */
 
 var db = require("mongojs").connect("nodebot", ["logs"]);
+var ent = require("ent");
 
 exports.index = function(req, res){
-	db.logs.find(function(err, data) {
+	db.logs.find({channel: '#channel'}, function(err, data) {
+		for (var i = 0; i < data.length; i++) {
+			data[i].log.message = ent.decode(data[i].log.message);
+		}
 		res.render('index',
 		  	{
 		  		title: 'Channel logs',
