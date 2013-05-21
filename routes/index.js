33068672a8166c20e983f114594b791d1e346d75
ernie <ernie@epicbytes.net>
2013-05-21 09:10:37 +0200

    trying out color thing

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,10 @@ exports.index = function(req, res){
 	db.logs.find({channel: Config.filter}, function(err, data) {
 		for (var i = 0; i < data.length; i++) {
 			data[i].log.message = ent.decode(data[i].log.message);
+			data[i].log.color = '';
+			for (var j = 0; i < 3; i++) {
+				data[i].log.color += data[i].log.nick.charCodeAt(i);
+			};
 		}
 		res.render('index',
 		  	{
