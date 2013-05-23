0bbfd6d95e556d5a4c3c6cd353fb0981e6830af6
ernie <ernie@epicbytes.net>
2013-05-23 11:13:24 +0200

    removed start-stop-daemon in favor for forever and added some console logs for debugging

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ var express = require('express')
   , server = http.createServer(app)
   , io = require('socket.io').listen(server)
   , Config = require('./config.js').Config
-  , startStopDaemon = require('start-stop-daemon')
   , moment = require("moment")
   , db = require("mongojs").connect(Config.databaseName, Config.databaseCollections)
   , md5 = require("MD5")
@@ -34,7 +33,13 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-function startSocket(lastId) {
+server.listen(app.get('port'), function(){
+	console.log('Express server listening on port ' + app.get('port'));
+});
+
+db.logs.find({channel: Config.filter}).sort({$natural:-1}).limit(1, function(err, data) {
+	var lastId = data[0]._id;
+	console.log(lastId);
 	io.sockets.on('connection', function(socket) {
 		setInterval(function() {
 	    	db.logs.find({channel: Config.filter, _id: {$gt: lastId}}, function(err, data) {
@@ -44,24 +49,13 @@ function startSocket(lastId) {
 						data[i].color = md5(data[i].nick).substring(1,7);
 					}
 					lastId = data[data.length-1]._id;
+					console.log(lastId);
 	    			socket.emit("logs", data);
+	    			console.log(data);
 	    		}
 	    	});
 	    }, 1000);
 	});
-}
-
-startStopDaemon(function() {
-
-	server.listen(app.get('port'), function(){
-		console.log('Express server listening on port ' + app.get('port'));
-	});
-
-	db.logs.find({channel: Config.filter}).sort({$natural:-1}).limit(1, function(err, data) {
-		if(!err) {
-			startSocket(data[0]._id);
-		}
-	});
-
-	app.get('/', routes.index);
 });
+
+app.get('/', routes.index);
