8073a18b23aab2038a9c333109d0c1ff9e925d22
ernie <ernie@epicbytes.net>
2013-05-25 00:32:22 +0200

    added date changing of log

--- app.js
+++ app.js
@@ -8,9 +8,11 @@ var express = require('express')
   , Config = require('./config.js').Config
   , db = require("mongojs").connect(Config.databaseName, Config.databaseCollections)
   , md5 = require("MD5")
-  , ent = require("ent");
+  , ent = require("ent")
+  , moment = require("moment");
 
 var lastId;
+var isToday = true;
 
 function parseData(data) {
 	for (var i = 0; i < data.length; i++) {
@@ -41,11 +43,21 @@ function emitLogData(socket, limit) {
 	});
 }
 
+function emitLogDataOnDate(socket, from, to) {
+	console.log(from, to);
+	db.logs.find({channel: Config.filter, date: {$gte: from, $lt: to}}, function(err, data) {
+		if(isToday) {
+			lastId = data[data.length-1]._id;
+		}
+		socket.emit("date logs", parseData(data));
+	});
+}
+
 function emitNewLogData(socket) {
 	db.logs.find({channel: Config.filter, _id: {$gt: lastId}}, function(err, data) {
 		if(data.length > 0) {
 			lastId = data[data.length-1]._id;
-			socket.emit("logs", parseData(data));
+			socket.emit("new logs", parseData(data));
 			emitChattyData(socket);
 		}
 	});
@@ -75,8 +87,24 @@ io.sockets.on('connection', function(socket) {
 	emitLogData(socket, 500);
 	emitChattyData(socket);
 
+	socket.on('date change', function(date) {
+		var now = moment();
+		var from = moment(date, "YYYY/MM/DD HH:mm:ss");
+		var to = from.clone().add('days', 1);
+
+		if(now.diff(from, 'days') == 0) {
+			isToday = true;
+		} else {
+			isToday = false;
+		}
+
+		emitLogDataOnDate(socket, from.format("YYYY/MM/DD HH:mm:ss"), to.format("YYYY/MM/DD HH:mm:ss"));
+	});
+
 	setInterval(function() {
-		emitNewLogData(socket);
+		if(isToday) {
+			emitNewLogData(socket);
+		}
 	}, 1000);
 });
 
--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -2,11 +2,44 @@ function scrollToBottom() {
 	$("#logs").scrollTop($("#logs")[0].scrollHeight);
 }
 
+function scrollToTop() {
+	$("#logs").scrollTop(0);
+}
+
 $(document).ready(function() {
+	var socket = io.connect('http://localhost:3000');
+
 	scrollToBottom();
 
-	var socket = io.connect('http://localhost:3000');
+	$("#datepicker").datepicker({ 
+	    dateFormat: "yy/mm/dd",
+	    defaultDate: 0,
+	    maxDate: 1,
+	    changeMonth: true,
+	    changeYear: true,
+	    onSelect: function(dateText, inst) {
+	        var date = $.datepicker.formatDate("yy/mm/dd 00:00:00", $('#datepicker').datepicker('getDate'));
+	        socket.emit("date change", date);
+        }
+    });
+
+    socket.on('date logs', function(data) {
+  		$("#logs").html('');
+    	for (var i = 0; i < data.length; i++) {
+    		$("#logs").append('<li><span class="date">' + data[i].date + '</span> <span style="color: #' + data[i].color + '">' + data[i].nick + "</span>: " + data[i].message + '</li>');
+    	}
+    	scrollToTop();
+  	});
+
   	socket.on('logs', function(data) {
+  		$("#logs").html('');
+    	for (var i = 0; i < data.length; i++) {
+    		$("#logs").append('<li><span class="date">' + data[i].date + '</span> <span style="color: #' + data[i].color + '">' + data[i].nick + "</span>: " + data[i].message + '</li>');
+    	}
+    	scrollToBottom();
+  	});
+
+  	socket.on('new logs', function(data) {
     	for (var i = 0; i < data.length; i++) {
     		$("#logs").append('<li><span class="date">' + data[i].date + '</span> <span style="color: #' + data[i].color + '">' + data[i].nick + "</span>: " + data[i].message + '</li>');
     	}
